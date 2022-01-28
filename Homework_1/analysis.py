077123772e979f98fb55f9fae58b9880a8ddcb42
surclxiii <bugey2540@gmail.com>
2022-01-28 00:30:06 -0800

    test

--- Homework_1/analysis.py
+++ Homework_1/analysis.py
@@ -1,9 +1,28 @@
+import csv
+
+
+def load_data_from_file(path):
+    #  Takes a file path as a string, returns two lists
+    data = open(path, 'r')
+    file = csv.DictReader(data)
+    time = []
+    position = []
+    for col in file:
+        time.append(col['Time'])
+        position.append(col['Position'])
+    print('Time:', time)
+    print('Position:', position)
+load_data_from_file('data1.csv')
+
 def greater_than_index(l, n):
     for i in l:
+        # Check if the element is greater than or equal to n
         if i >= n:
+            # Return the index
             return l.index(i)
     return
 
-list = [1.1, 2.2, 3.3, 4.4, 5.5]
+
+list = [1, 3, 4, 7, 10]
 print(list)
-print(greater_than_index(list, 100.5))
+print(greater_than_index(list, 6))
