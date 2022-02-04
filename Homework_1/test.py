da25542ae09d21c43c1c70b3e832fb54f1e7823f
surclxiii <bugey2540@gmail.com>
2022-02-04 12:09:42 -0800

    3Feb

--- Homework_1/test.py
+++ Homework_1/test.py
@@ -1,18 +1,21 @@
 import csv
+import math
 
 
+# Work with Ittiwat Poonprachasin
 # Problem 1
 def load_data_from_file(path):
     # Takes a file path as a string, returns two lists
-    filename = open(path)
-    reader = csv.reader(filename)
+    file = open(path, 'r')
+    # Read only
+    reader = csv.reader(file)
     header = next(reader)
     time = []
     position = []
     for row in reader:
         time.append(float(row[0]))
         position.append(float(row[1]))
-    filename.close()
+    file.close()
     return time, position
 
 
@@ -29,80 +32,83 @@ def greater_than_index(l, n):
 def estimates_value(path):
     # Take time and position from load data function
     time, position = load_data_from_file(path)
-    # Determine maximum, initial, final position
-    c_max = max(position)
+    # Determine initial, maximum, final position
     c_initial = position[0]
+    c_max = max(position)
     c_final = position[-1]
-    return time, position, c_max, c_initial, c_final
+    return time, position, c_initial, c_max, c_final
 
 
 # Problem 3
 def estimate_char(path):
     # Take values from estimates_value to estimate characteristic
-    time, position, c_max, c_initial, c_final = estimates_value(path)
-    # Determine outputs
+    time, position, c_initial, c_max, c_final = estimates_value(path)
+    # 10% time
     p_ten = ((c_final - c_initial) * 0.1) + c_initial
+    p_ten_i = greater_than_index(position, p_ten)
+    time_ten = time[p_ten_i]
+    # 90% time
     p_ninety = ((c_final - c_initial) * 0.9) + c_initial
-    p_diff = p_ten - p_ninety
-    p_ten_index = greater_than_index(position, p_ten)
-    p_ninety_index = greater_than_index(position, p_ninety)
-    time_ten = time[p_ten_index]
-    time_ninety = time[p_ninety_index]
+    p_ninety_i = greater_than_index(position, p_ninety)
+    time_ninety = time[p_ninety_i]
     # Rise time
-    time_r = time_ninety - time_ten
-    p_peak_index = greater_than_index(position, c_max)
+    t_r = time_ninety - time_ten
+    p_peak_i = greater_than_index(position, c_max)
     # Peak time
-    time_p = time[p_peak_index]
+    t_p = time[p_peak_i]
     # Percentage overshoot
-    percent_overshoot = (abs((c_max - c_final) / (c_initial - c_final)) * 100) - 100
-    pos_ini_index = greater_than_index(position, (c_initial - p_diff) * 0.02)
-    pos_final_index = greater_than_index(position, (c_final - p_diff) * 0.02)
-    time_ini_two = time[pos_ini_index]
-    time_final_two = time[pos_final_index]
+    percent_overshoot = (abs((c_max - c_final) / (c_final - c_initial)) * 100)
+    bounds = 0.02 * (abs(c_initial) + abs(c_final))
+    upper = greater_than_index(position, (c_final + bounds))
+    lower = greater_than_index(position, (c_final - bounds))
+    time_up = time[upper]
+    time_low = time[lower]
     # Settling time
-    time_s = abs(time_ini_two - time_final_two)
-    mass = 1
-    return time_r, time_p, percent_overshoot, time_s, mass
+    T_s = time_up - time_low
+    return t_r, t_p, percent_overshoot, T_s
 
 
 # PROBLEM 4
-def get_system_params(percent_overshoot, time_s):
-    # False declare to make the code rune
-    s = 1
-    w_n = 4/(s * time_s)
-    spring_k = w_n ** 2
-    damping_factor = w_n * 2 * s
-    mass = 1
-    return mass, spring_k, damping_factor
+def get_system_params(percent_overshoot, T_s):
+    zeta = (-math.log(percent_overshoot / 100)) / math.sqrt((math.pi ** 2) + (math.log(percent_overshoot / 100)) ** 2)
+    w_n = 4 / (zeta * T_s)
+    k = w_n ** 2
+    c = w_n * 2 * zeta
+    m = 1
+    return k, c, m
 
 
 # Problem 5
 def analyze_data(path):
-    time, position, c_max, c_initial, c_final = estimates_value(path)
-    time_r, time_p, percent_overshoot, time_s, mass = estimate_char(path)
-    mass, spring_k, damping_factor = get_system_params(percent_overshoot, time_s)
-    dict1 = {"c_initial": c_initial, "c_final": c_final, "c_max": c_max, "rise_time": time_r, "peak_time": time_p,
-            "percent_overshoot": percent_overshoot, "settling_time": time_s, "system_mass": mass,
-            "system_spring": spring_k, "system_damping": damping_factor}
-    sort_dict = sorted(dict1.items())
-    for i, j in sort_dict:
-        print(i, j)
+    time, position, c_initial, c_max, c_final = estimates_value(path)
+    t_r, t_p, percent_overshoot, T_s = estimate_char(path)
+    k, c, m = get_system_params(percent_overshoot, T_s)
+    unsorted_dict = {'c_initial': c_initial, 'c_max': c_max, 'c_final': c_final, 'rise_time': t_r,
+                     'peak_time': t_p, 'perc_overshoot': percent_overshoot, 'settling_time': T_s, 'system_mass': m,
+                     'system_spring': k, 'system_damping': c}
+    return unsorted_dict
 
 
 if __name__ == '__main__':
     path = 'data1.csv'
-    print(analyze_data(path))
+    data = analyze_data(path)
+    sorted_data = sorted(data.items())
+    for key, value in sorted_data:
+        print(key, value)
+    # print(load_data_from_file(path))
+    # print(analyze_data(path))
     # list = [1, 3, 4, 7, 10]
     # print(list)
     # print(greater_than_index(list, 6))
     # time, position, c_max, c_initial, c_final = estimates_value('data1.csv')
-    # time_r, time_p, percent_overshoot, time_s, mass = estimate_char('data1.csv')
-    # mass, spring_k, damping_factor = get_system_params(percent_overshoot, time_s)
+    # t_r, t_p, percent_overshoot, T_s = estimate_char('data1.csv')
+    # mass, k, c = get_system_params(percent_overshoot, T_s)
     # print('c_initial:', c_initial)
     # print('c_max:', c_max)
     # print('c_final:', c_final)
-    # print('timr_r:', time_r)
-    # print('time_p:', time_p)
-    # print('time_s:', time_s)
+    # print(get_system_params(percent_overshoot, T_s))
+    # print('timr_r:', t_r)
+    # print('t_p:', t_p)
+    # print('T_s:', T_s)
     # print('percent_overshoot:', percent_overshoot)
     # print('mass:', mass)
