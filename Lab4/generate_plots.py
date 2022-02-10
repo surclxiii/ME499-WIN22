d1c8d36c415498d8818381f1e20167e48444e169
surclxiii <bugey2540@gmail.com>
2022-02-09 17:11:56 -0800

    9Feb

--- Lab4/generate_plots.py
+++ Lab4/generate_plots.py
@@ -1,25 +1,44 @@
-import msd
-import utils
-import matplotlib
-matplotlib.use('TkAgg')
+import counter
 import matplotlib.pyplot as plt
+import msd
+import time
+import utils
 
 
 if __name__ == '__main__':
-    smd = msd.MassSpringDamper(m=1.0, k=5.0, c=2.5)
-    state, t = smd.simulate(1.0, -1.0, 40, 0.01)
+    # Problem 1: Plot spring damper system
+    smd = msd.MassSpringDamper(m=1.0, k=5.0, c=2.5)  # mass of 1.0, spring constant = 5.0, spring damper = 2.5
+    state, t = smd.simulate(1.0, -1.0, 40, 0.01)  # start: 1.0, initial velocity: -1.0, time: 40sec, increment: 0.01
     plt.figure(1)
-    plt.plot(t, state)
-    plt.xlim([0, 10])
+    plt.plot(t, state)  # Plot time vs position
+    plt.xlim([0, 10])  # X-axes from 0 to 10
     plt.title("Spring Damper System")
     plt.xlabel("time (s)")
     plt.ylabel("Position (m)")
+    plt.savefig('Problem1.png')
     plt.show()
-    plt.clf()
-    result = []
-    for run_times in range(1000):
-        result.append(utils.simulate_gachapon(15))
+    # Problem 2: Histogram of the gachapon problem
+    result = []  # new result list
+    for run_times in range(1000):  # Run simulation for 1000 times
+        result.append(utils.simulate_gachapon(no_prize=15))  # Save result to the list
     plt.figure(2)
-    n, bins, patches = plt.hist(result, 50, density=1, facecolor='g', alpha=0.75)
+    plt.hist(result, 40)  # Plot histrogram with bin size: 40
+    plt.xlim(left=0)  # X-axes starts from 0
+    plt.title("Percent of Boxes of cereal to get 15 prizes (bins=40)")
+    plt.xlabel("Boxes ")
+    plt.ylabel("Percent")
+    plt.savefig('Problem2.png')
     plt.show()
-    plt.clf()
+    # Problem 3: Algorithmic runtimes
+    for value in range(50, 2550, 50):  # For all values 50, 100, ... up to 2500
+        time_1 = time.time()  # Start counter
+        list_random = utils.random_list(value)  # Get random list
+        list_element_count = counter.get_element_counts(list_random)  # Count elements in random list
+        time_2 = time.time()  # Stop counter
+        delta_time = time_2 - time_1
+        plt.scatter(value, delta_time)
+    plt.title("List Lengths vs. Runtimes")
+    plt.xlabel("List Lengths")
+    plt.ylabel("Runtime (sec)")
+    plt.savefig('Problem3.png')
+    plt.show()
